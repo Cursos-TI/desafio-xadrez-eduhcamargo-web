8acb5e09f3c3871018211c67d13c89ef5229ecc0
LUIZ EDUARDO DAMASCENO CAMARGO <eduhcamargo@gmail.com>
2025-11-10 15:13:00 +0000

    Atualização nível NOVATO

--- xadrez.c
+++ xadrez.c
@@ -8,15 +8,58 @@ int main() {
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
 
+    int movimento_torre = 5;
+    int movimento_bispo = 5;
+    int movimento_rainha = 8;
+
+
     // Implementação de Movimentação do Bispo
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
 
+    printf("___ Movimento do Bispo ___"); //While
+    printf("Simulando %d casas na diagonal (Cima e Direita):\n", movimento_bispo);
+    
+    int contador_bispo = 1;
+
+    //loop
+    while (contador_bispo <= movimento_bispo)
+    {
+        printf("Cima-Direita\n");
+        contador_bispo++;
+    }
+
+    printf("\n");
+    
     // Implementação de Movimentação da Torre
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
+    
+    printf("___ Movimento da torre ___"); //Usando o 'For'
+    printf("Simulando %d casas para a direita: \n", movimento_torre);
+
+    //loop
+    for (int i = 1; i <= movimento_torre; i++)
+    {
+        printf("Direita\n");
+    }
+
+    printf("\n");
 
     // Implementação de Movimentação da Rainha
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
 
+    printf("___ Movimento da Rainha ___"); //do-while
+    printf("Simulando %d casa para a esquerda:\n", movimento_rainha);
+
+    int contador_rainha = 1;
+
+    //loop
+    do{
+        printf("Esqueda\n");
+        contador_rainha++;
+    } while (contador_rainha <= movimento_rainha);
+    
+    printf("\n");
+    
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
     // Um loop pode representar a movimentação horizontal e outro vertical.
